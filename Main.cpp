10ec627e5f60a7ec73b61aa92094c753b7496ecb
Piotr Czapla <piotr.czapela@gmail.com>
2022-03-14 23:37:39 +0100

    qwe

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	glfwInit();
 	//newcomment
-
+	//onemore comment
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
